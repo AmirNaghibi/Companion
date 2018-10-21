6235aa4554591e3ba8e340057eba43d41c62b0df
Amir Naghibi <amirmahdi76@gmail.com>
2018-10-20 20:03:29 -0700

    added static file support to urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/dashboard/')),
     path('dashboard/', include('dashboard.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
