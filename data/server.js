8accf3a44fe0890a2c9d58f8704152aad8ef0a81
Harry Yao <harryyao13@gmail.com>
2018-10-21 13:05:23 -0700

    old backend code

---
+++ data/server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var router = express.Router();
+
+var CrimeData = require("./CrimeData.json");
+
+router.get("/crime", function(req, res, next) {
+	console.log("cds")
+	res.json(CrimeData);
+});
+
+app.get('/crimedata', (req, res) => {
+	console.log("Crime data sent")
+	res.send(CrimeData)
+})
+
+const PORT = process.env.PORT || 3000;
+	
+var server = http.listen(PORT, () => {
+    console.log('server is listening on port ', server.address().port)
+})
