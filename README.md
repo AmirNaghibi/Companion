5a24b911a0c29b0b314d3f91b157ea486c483c37
John Lee <jlee14c@hotmail.com>
2018-10-21 03:10:21 -0700

    google maps improvements

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ find the safest way back home with a trusted
 
 # Frontend
 ```
+// to develop locally
 npm install
 npm start
+
+// to build
+npm run build
 ```
 
-
-
+If you have the backend API deployed somewhere with the endpoints available. We can simply call that API endpoint through a regular http request.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,34 @@ import { PAGE_MAP, PAGE_CHAT } from './constants';
 import ChatPage from './chat/Chat.js'
 import MapPage from './map/Map.js'
 
+const TEST_PATH = [
+  { lat: 47.65641, lng: -122.3132624 },
+  { lat: 47.6575286, lng: -122.3141852 },
+  { lat: 47.6594009, lng: -122.3161383 },
+  { lat: 47.6609129, lng: -122.3192261 },
+  { lat: 47.6626159, lng: -122.3165597 },
+];
+
+const TEST_CRIME_DATA = [
+  { Latitude: 47.65851470026042, Longitude: -122.31498548014832, Description: 'SHOPLIFTING', Date: '05/05/2017 06:40:32 PM' },
+  { Latitude: 47.65903499736511, Longitude: -122.3180968426056, Description: 'THEFT - CAR PROWL', Date: '06/05/2017 08:30:02 PM' },
+  { Latitude: 47.66119643672335, Longitude: -122.31405258178711, Description: 'SUSPICIOUS CIRCUMSTANCES', Date: '19/05/2017 03:30:33 AM' },
+];
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: PAGE_MAP
+      page: PAGE_MAP,
+      path: TEST_PATH || [],
+      currentLocation: null,
+      destination: null,
+      crimeData: TEST_CRIME_DATA || []
     };
     this.onRoute = this.onRoute.bind(this);
+    this.onUpdateCurrentLocation = this.onUpdateCurrentLocation.bind(this);
+    this.onUpdateDestination = this.onUpdateDestination.bind(this);
+    this.onUpdateCrimeData = this.onUpdateCrimeData.bind(this);
   }
 
   onRoute(page) {
@@ -20,6 +41,37 @@ class App extends Component {
     }
   }
 
+  onUpdateCurrentLocation(currentLocation) {
+    console.log('current location is ', currentLocation);
+    if (currentLocation && currentLocation.lat && currentLocation.lng) {
+      this.setState({ currentLocation })
+    } else {
+      console.error('ERROR: unable to update current location');
+      this.setState({ currentLocation: null });
+    }
+  }
+
+  onUpdateDestination(e) {
+    const destination = {
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
+    };
+    console.log(destination);
+    if (destination && destination.lat && destination.lng) {
+      this.setState({ destination });
+    } else {
+      console.error('ERROR: unable to update destination');
+      this.setState({ destination: null });
+    }
+  }
+
+  onUpdateCrimeData(data) {
+    // TODO - add an API call here to retrieve the crime data from the server
+    console.log('updating crime data...');
+    const crimeData = TEST_CRIME_DATA;
+    this.setState({ crimeData });
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,10 +79,20 @@ class App extends Component {
         <div style={{ height: 40, width: '100%', backgroundColor: '#c1c1c1'}}>HEADER</div>
 
         {/* Maps Page */}
-        {(this.state.page === PAGE_CHAT) && <ChatPage onRoute={this.onRoute}/>}
+        {(this.state.page === PAGE_CHAT) && <ChatPage
+          onRoute={this.onRoute}
+        />}
 
         {/* Chat Page */}
-        {(this.state.page === PAGE_MAP) && <MapPage onRoute={this.onRoute}/>}
+        {(this.state.page === PAGE_MAP) && <MapPage
+          onUpdateCurrentLocation={this.onUpdateCurrentLocation}
+          onMapClick={this.onUpdateDestination}
+          currentLocation={this.state.currentLocation}
+          crimeData={this.state.crimeData}
+          destination={this.state.destination}
+          path={this.state.path}
+          onRoute={this.onRoute}
+        />}
       </div>
     )
   }
